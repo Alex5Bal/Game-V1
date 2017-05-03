458079a42ee64088172536eb48def70393760b0c
Alex5Bal <abalderrama5@outlook.com>
2017-05-03 01:40:21 -0600

    Add files via upload

--- game/buzzer.c
+++ game/buzzer.c
@@ -38,40 +38,32 @@ void buzzer_set_period(short cycles){
 void gameOverSong(){
   switch(counter){
   	  case 0:
-  		  buzzer_set_period(900); counter++;
-  		  break; //Lower C note
+  		  buzzer_set_period(500); counter++;
+  		  break;
   	  case 1:
-  		  buzzer_set_period(400); counter++;
-  		  break; //C note
-  	  /*case 2:
-  		  buzzer_set_period(700); counter++;
-  		   break; //G note
-  	  case 3:
-  		  buzzer_set_period(200); counter++;
-  		   break; //E note
-  	  case 4:
-  		  buzzer_set_period(800); counter++;
-  		  break; //F note*/
+  		  buzzer_set_period(0); counter++;
+  		  break;
   	  case 2:
-  		  buzzer_set_period(700);
+  		  buzzer_set_period(700); counter++;
+  		   break;
+  	  case 3:
+  		  buzzer_set_period(0); counter++;
+  		   break;
+  	  case 4:
+  		  buzzer_set_period(300); counter++;
+  		  break;
+  	  case 5:
+  		  buzzer_set_period(0); counter++;
+  		  break;
+  	  case 6:
+  		  buzzer_set_period(200);
 
-  		  	  if(counter == 2) {
+  		  	  if(counter == 6) {
   		  		  counter = 0;
   		  	  }
   		  	  else {
   		  		  counter++;
   		  	  };
-  		  	  break;//D note
-  	/*  case 6:
-  	  case 7:
-  	  case 8:
-  	  case 9:
-  	  case 10:
-  	  case 11:
-  	  case 12:
-  	  case 13:
-  	  case 14:
-  	  case 15:*/
-
+  		  	  break;
   }
 }
